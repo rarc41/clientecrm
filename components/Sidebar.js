dd7dc672ca9ac9c88e8cd530eb7b87d0f558180b
Robin Angel Romero Cordero <ingrobinromeo@gmail.com>
2021-12-08 17:57:54 -0500

    terminando sidebar, detecatando pagina activa

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React from "react";
+import Link from "next/link";
+import {useRouter} from 'next/router'
 
 const Sidebar = () => {
-    return (
-        <aside className="bg-gray-800 sm:w-1/3 xl:w-1/5 sm:min-h-screen p-5">
-            <div>
-                <p className="text-white text-2xl font-black"> CRM Clientes</p>
-            </div>
-        </aside>
-    );
+
+    // routing de nextjs
+    const router = useRouter()
+    console.log(router.pathname)
+  return (
+    <aside className="bg-gray-800 sm:w-1/3 xl:w-1/5 sm:min-h-screen p-5">
+      <div>
+        <p className="text-white text-2xl font-black"> CRM Clientes</p>
+      </div>
+
+      <nav className="mt-5 list-none h-48">
+        <li className={router.pathname==="/" ? "bg-blue-800 p-3": "p-3"}>
+          <Link href="/">
+            <a className="text-white mb-3 block">Clientes</a>
+          </Link>
+        </li>
+        <li className={router.pathname==="/pedidos" ? "bg-blue-800 p-3": "p-3"}>
+          <Link href="/pedidos">
+            <a className="text-white mb-3 block">Pedidos</a>
+          </Link>
+        </li>
+        <li className={router.pathname==="/productos" ? "bg-blue-800 p-3": "p-3"}>
+          <Link href="/productos">
+            <a className="text-white mb-3 block">Productos</a>
+          </Link>
+        </li>
+      </nav>
+    </aside>
+  );
 };
 
 export default Sidebar;
