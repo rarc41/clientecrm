5cf9866746c0d2d7ba63f85f76c3e4427bcc44bd
Robin Angel Romero Cordero <ingrobinromeo@gmail.com>
2021-12-08 19:14:06 -0500

    formulario para crear nueva cuenta

--- pages/nuevacuenta.js
+++ pages/nuevacuenta.js
@@ -5,7 +5,76 @@ const NuevaCuenta = () => {
   return (
     <>
       <Layout>
-        <h1>Nueva Cuenta</h1>
+        <h1 className="text-center text-2xl text-white font-light">
+          Crear Nueva Cuenta
+        </h1>
+        <div className="flex justify-center mt-5">
+          <div className="w-full max-w-sm">
+            <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="nombre"
+                >
+                  Nombre
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:ring-2 focus:border-blue-300 focus:border-transparent"
+                  id="nombre"
+                  type="text"
+                  placeholder="Nombre"
+                ></input>
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="apellido"
+                >
+                  Apellido
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:ring-2 focus:border-blue-300 focus:border-transparent"
+                  id="apellido"
+                  type="text"
+                  placeholder="Apellido"
+                ></input>
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="email"
+                >
+                  Email
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:ring-2 focus:border-blue-300 focus:border-transparent"
+                  id="email"
+                  type="email"
+                  placeholder="Email Usuario"
+                ></input>
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="password"
+                >
+                  Password
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:ring-2 focus:border-blue-300 focus:border-transparent"
+                  id="password"
+                  type="password"
+                  placeholder="Password"
+                ></input>
+              </div>
+              <input
+                type="submit"
+                className="bg-gray-800 w-full mt-5 p-2 text-white uppercase font-bold hover:bg-gray-900"
+                value="Crear Cuenta"
+              ></input>
+            </form>
+          </div>
+        </div>
       </Layout>
     </>
   );
